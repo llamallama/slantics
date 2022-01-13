7269c77d86bffc57ee55aad303366705909639f4
Chris Jones <chris@sysengineerchris.com>
2022-01-13 17:38:13 -0500

    More two dimension array stuff

--- main.py
+++ main.py
@@ -32,6 +32,21 @@ SHAPES = {
         [1, 2, 0],
         [5, 0, 3],
         [0, 0, 4],
+    ],
+    "bit": [
+        [0, 0, 0],
+        [1, 0, 0],
+        [3, 2, 0],
+    ],
+    "corner": [
+        [0, 1, 0],
+        [5, 0, 0],
+        [4, 3, 2],
+    ],
+    "crux": [
+        [0, 0, 0],
+        [0, 1, 0],
+        [2, 3, 4],
     ]
 }
 
@@ -49,7 +64,6 @@ class Slantic(object):
         self.color_dark = BLUE
         self.mode = "dark"
 
-        print(self.shape)
 
         self.coords = [
             [(0,0),              (self.width/2, 0),             (self.width, 0)],
@@ -57,7 +71,7 @@ class Slantic(object):
             [(0, self.height),   (self.width/2, self.height),   (self.width, self.height)]
         ]
 
-        self.drawSlantic()
+        self.rect = self.drawSlantic()
 
 
     def drawSlantic(self):
@@ -77,8 +91,9 @@ class Slantic(object):
                     continue
 
         self.surface.fill(fill_color)
-        pygame.draw.polygon(self.surface, poly_color, poly_list)
+        rect = pygame.draw.polygon(self.surface, poly_color, poly_list)
         self.screen.blit(self.surface, (self.x + 1, self.y + 1))
+        return rect
 
 
 def main():
@@ -89,25 +104,38 @@ def main():
     FPS = 30
     SCREEN.fill(WHITE)
 
-    slantics=[]
+    bar_r = Slantic(SHAPES["bar"], 0, 0, SCREEN)
+    bar_l = Slantic(np.fliplr(SHAPES["bar"]).tolist(), 1, 0, SCREEN)
+    beam_r = Slantic(SHAPES["beam"], 2, 0, SCREEN)
+    beam_l = Slantic(np.fliplr(SHAPES["beam"]).tolist(), 3, 0, SCREEN)
+    bit = Slantic(SHAPES["bit"], 4, 0, SCREEN)
+    corner_r = Slantic(SHAPES["corner"], 5, 0, SCREEN)
+    corner_l = Slantic(np.fliplr(SHAPES["corner"]).tolist(), 6, 0, SCREEN)
+    crux = Slantic(SHAPES["crux"], 7, 0, SCREEN)
 
-    slantics.append(Slantic(SHAPES["bar"], 0, 0, SCREEN))
-    slantics.append(Slantic(np.fliplr(SHAPES["bar"]).tolist(), 1, 0, SCREEN))
-    slantics.append(Slantic(SHAPES["beam"], 2, 0, SCREEN))
-    slantics.append(Slantic(np.fliplr(SHAPES["beam"]).tolist(), 3, 0, SCREEN))
+    slantics = [bar_r, bar_l, beam_r, beam_l, bit, corner_r, corner_l, crux]
+
 
     while True:
+        pygame.display.update()
         SCREEN.fill(WHITE)
         drawGrid()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
 
         for s in slantics:
             s.drawSlantic()
 
-        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if bar_r.rect.collidepoint(pygame.mouse.get_pos()):
+                    print('bar_r')
+
+                if bar_l.rect.collidepoint(pygame.mouse.get_pos()):
+                    print('bar_l')
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
         CLOCK.tick(FPS)
 
 
