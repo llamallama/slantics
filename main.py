5e88aad5ff3ebfff5e4dc2fc0cfca7a36b3df9b8
Chris Jones <chris@sysengineerchris.com>
2022-01-15 21:23:23 -0500

    Adding the rest of the shapes

--- main.py
+++ main.py
@@ -26,27 +26,72 @@ SHAPES = {
     "bar": [
         [0, 0, 1],
         [4, 0, 2],
-        [3, 0, 0],
+        [3, 0, 0]
     ],
     "beam": [
         [1, 2, 0],
         [5, 0, 3],
-        [0, 0, 4],
+        [0, 0, 4]
     ],
     "bit": [
         [0, 0, 0],
         [1, 0, 0],
-        [3, 2, 0],
+        [3, 2, 0]
     ],
     "corner": [
         [0, 1, 0],
         [5, 0, 0],
-        [4, 3, 2],
+        [4, 3, 2]
     ],
     "crux": [
         [0, 0, 0],
         [0, 1, 0],
-        [2, 3, 4],
+        [2, 3, 4]
+    ],
+    "fang": [
+        [0, 0, 0],
+        [1, 0, 0],
+        [0, 3, 2]
+    ],
+    "hexx": [
+        [0, 1, 2],
+        [6, 0, 3],
+        [5, 4, 0]
+    ],
+    "hill": [
+        [0, 1, 0],
+        [6, 0, 2],
+        [5, 4, 3]
+    ],
+    "peak": [
+        [0, 1, 0],
+        [0, 0, 0],
+        [4, 3, 2]
+    ],
+    "point": [
+        [1, 0, 0],
+        [0, 0, 2],
+        [0, 4, 3]
+    ],
+    "slant": [
+        [0, 0, 1],
+        [0, 0, 2],
+        [5, 4, 3]
+    ],
+    "slope": [
+        [0, 0, 0],
+        [0, 0, 2],
+        [1, 4, 3]
+    ],
+    "spike": [
+        [1, 0, 0],
+        [0, 0, 2],
+        [0, 0, 3]
+    ],
+    "strip": [
+        [0, 1, 2],
+        [4, 0, 0],
+        [3, 0, 0]
     ]
 }
 
@@ -90,7 +135,7 @@ class Slantic(object):
         fill_color = self.color_light if self.dark else self.color_dark
         poly_color = self.color_dark if self.dark else self.color_light
 
-        for num in range(1,6):
+        for num in range(1,10):
             for index, s in enumerate(self.shape):
                 if num in s:
                     poly_list.append(self.coords[index][s.index(num)])
@@ -98,6 +143,7 @@ class Slantic(object):
 
         rect = pygame.draw.rect(self.surface, fill_color, (self.x, self.y, self.width, self.height))
         pygame.draw.polygon(self.surface, poly_color, poly_list)
+
         return rect
 
     def flip(self):
@@ -124,8 +170,41 @@ def main():
     corner_r = Slantic(SHAPES["corner"], 5, 0, SCREEN)
     corner_l = Slantic(np.fliplr(SHAPES["corner"]).tolist(), 6, 0, SCREEN)
     crux = Slantic(SHAPES["crux"], 7, 0, SCREEN)
+    fang_r = Slantic(SHAPES["fang"], 0, 1, SCREEN)
+    fang_l = Slantic(np.fliplr(SHAPES["fang"]).tolist(), 1, 1, SCREEN)
+    hexx = Slantic(SHAPES["hexx"], 2, 1, SCREEN)
+    hill = Slantic(SHAPES["hill"], 3, 1, SCREEN)
+    peak = Slantic(SHAPES["peak"], 4, 1, SCREEN)
+    point = Slantic(SHAPES["point"], 5, 1, SCREEN)
+    slant = Slantic(SHAPES["slant"], 6, 1, SCREEN)
+    slope_r = Slantic(SHAPES["slope"], 7, 1, SCREEN)
+    slope_l = Slantic(np.fliplr(SHAPES["slope"]).tolist(), 0, 2, SCREEN)
+    spike_r = Slantic(SHAPES["spike"], 1, 2, SCREEN)
+    spike_l = Slantic(np.fliplr(SHAPES["spike"]).tolist(), 2, 2, SCREEN)
+    strip = Slantic(SHAPES["strip"], 3, 2, SCREEN)
 
-    slantics = [bar_r, bar_l, beam_r, beam_l, bit, corner_r, corner_l, crux]
+    slantics = [
+        bar_r,
+        bar_l,
+        beam_r,
+        beam_l,
+        bit,
+        corner_r,
+        corner_l,
+        crux,
+        fang_r,
+        fang_l,
+        hexx,
+        hill,
+        peak,
+        point,
+        slant,
+        slope_r,
+        slope_l,
+        spike_r,
+        spike_l,
+        strip
+    ]
 
 
     while True:
