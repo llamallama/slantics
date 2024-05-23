f2928ffe331535f571665c0d8e38c114131f7ec0
Chris Jones <chris@sysengineerchris.com>
2024-05-23 15:32:40 -0400

    Rotation works

--- main.py
+++ main.py
@@ -10,16 +10,13 @@ clock = pygame.time.Clock()
 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos=(0, 0), dimensions=(50, 50),  color='black'):
+    def __init__(self, pos=(0, 0), dimensions=(100, 100),  color='black'):
         super().__init__()
-        self.image = pygame.Surface(dimensions)
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-        self.rect.move_ip(pos)
+        # self.image = pygame.Surface(dimensions)
+        self.image = pygame.image.load('tile.png').convert_alpha()
+        # self.image.fill(color)
+        self.rect = self.image.get_rect(topleft=pos)
         self.dragging = False
-
-    def click(self):
-        print('here')
 
     def drag(self, events):
         for event in events:
@@ -30,8 +27,14 @@ class Tile(pygame.sprite.Sprite):
             if self.dragging and event.type == pygame.MOUSEMOTION:
                 self.rect.move_ip(event.rel)
 
+    def rotate(self, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r and self.rect.collidepoint(pygame.mouse.get_pos()):
+                self.image = pygame.transform.rotate(self.image, -90)
+
     def update(self, events):
         self.drag(events)
+        self.rotate(events)
 
 
 tile_group = pygame.sprite.Group()
